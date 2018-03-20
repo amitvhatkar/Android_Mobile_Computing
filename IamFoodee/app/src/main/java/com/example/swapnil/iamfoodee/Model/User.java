29ea4a9665a7254086bd7bf7994cb97b40ddb795
amitvhatkar <asvhatkar@gmail.com>
2018-03-20 11:02:43 +0530

    Intermidiate: Storing order

--- IamFoodee/app/src/main/java/com/example/swapnil/iamfoodee/Model/User.java
+++ IamFoodee/app/src/main/java/com/example/swapnil/iamfoodee/Model/User.java
@@ -7,16 +7,17 @@ package com.example.swapnil.iamfoodee.Model;
 public class User {
     private String Name;
     private String Password;
+    private String Phone;
 
     public User(){
 
     }
 
-    public User(String name,String password){
-        Name=name;
-        Password=password;
+    public User(String name, String password, String phone) {
+        Name = name;
+        Password = password;
+        Phone = phone;
     }
-
 
     public String getName() {
         return Name;
@@ -32,5 +33,13 @@ public class User {
 
     public void setPassword(String password) {
         Password = password;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
     }
 }
