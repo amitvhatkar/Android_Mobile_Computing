1d724f6092e6a8c1dcaaad385d0f6b7dc8785ee7
amitvhatkar <asvhatkar@gmail.com>
2018-03-21 17:05:03 +0530

    Added functionality to navigation drawer

---
+++ IamFoodee/app/src/main/java/com/example/swapnil/iamfoodee/ViewHolder/OrderViewHolder.java
@@ -0,0 +1,38 @@
+package com.example.swapnil.iamfoodee.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.swapnil.iamfoodee.Interface.ItemClickListener;
+import com.example.swapnil.iamfoodee.R;
+
+/**
+ * Created by asvhatkar on 21/3/18.
+ */
+
+public class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView txtOrderId, txtOrderStatus, txtOrderPhone;
+    private ItemClickListener itemClickListener;
+
+    public OrderViewHolder(View itemView) {
+        super(itemView);
+
+        txtOrderId = (TextView)itemView.findViewById(R.id.order_id);
+        txtOrderPhone= (TextView)itemView.findViewById(R.id.order_phone);
+        txtOrderStatus= (TextView)itemView.findViewById(R.id.order_status);
+
+        itemView.setOnClickListener(this);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+            //itemClickListener.onClick(view, getAdapterPosition(), false);
+    }
+}
